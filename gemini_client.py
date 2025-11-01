b9f78c28df79c86368051bda0cea67a43c87da16
Chandana123201 <chandumgowda02@gmail.com>
2025-11-01 19:38:38 +0530

    Updated code and removed API key

--- gemini_client.py
+++ gemini_client.py
@@ -2,8 +2,12 @@ import os
 from google import genai
 from dotenv import load_dotenv
 
-# Load .env file
+# Load environment variables
 load_dotenv()
 
+# Get API key from .env file
 api_key = os.getenv("GOOGLE_API_KEY")
+
+# Initialize Gemini client
 client = genai.Client(api_key=api_key)
+
